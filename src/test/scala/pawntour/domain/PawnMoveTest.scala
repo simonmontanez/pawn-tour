b9c2d1660ec69af202560cc2ba2cddd1fdf6b1cc
Simón <simonmontanez@seven4n.com>
2018-08-28 06:12:42 -0500

    Test to pawn moves

--- src/test/scala/pawntour/domain/PawnMoveTest.scala
+++ src/test/scala/pawntour/domain/PawnMoveTest.scala
@@ -5,4 +5,19 @@ import org.scalatest.{Matchers, WordSpec}
 class PawnMoveTest extends WordSpec with Matchers {
   val board = new Chequerboard
 
+  "PawnMove" should {
+    "build an expected position" in {
+      val coordinate = Coordinate(5, 0)
+      N.position(coordinate) shouldBe Coordinate(2, 0)
+      NE.position(coordinate) shouldBe Coordinate(3, 2)
+      E.position(coordinate) shouldBe Coordinate(5, 3)
+      SE.position(coordinate) shouldBe Coordinate(7, 2)
+      S.position(coordinate) shouldBe Coordinate(8, 0)
+      SW.position(coordinate) shouldBe Coordinate(7, -2)
+      W.position(coordinate) shouldBe Coordinate(5, -3)
+      NW.position(coordinate) shouldBe Coordinate(3, -2)
+
+    }
+  }
+
 }
